2aae20cd6785c81833aa00bc57d98fe45f36c76a
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-25 18:58:31 -0300

    Comecei a criar a jornada do usuário Doador

--- frontend/src/Doador.js
+++ frontend/src/Doador.js
@@ -1,6 +1,36 @@
 import React from 'react'
 export default function Doador() {
-    return (
-      <h1>DOADOR</h1>
-    );
-  }
+  return (
+    <div>
+      <div className="text-center">
+        <h1>Como Doar</h1>
+        <h2>Entenda Como Você Pode Contribuir</h2>
+      </div>
+      <div className="container">
+        <div class="card-deck mb-3 text-center row">
+          <div className="card m-4 box-shadow col">
+            <div className="card-header">
+              <h4 class="my-0 font-weight-normal">Kit</h4>
+            </div>
+            <div className="card-body">
+            </div>
+          </div>
+          <div className="card m-4 box-shadow col">
+            <div className="card-header">
+              <h4 class="my-0 font-weight-normal">Outro valor</h4>
+            </div>
+            <div className="card-body">
+            </div>
+          </div>
+          <div className="card m-4 box-shadow col">
+            <div className="card-header">
+              <h4 class="my-0 font-weight-normal">ONG</h4>
+            </div>
+            <div className="card-body">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
