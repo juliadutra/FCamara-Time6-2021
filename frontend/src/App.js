ca8b359f7c3bd8589af750ac843edf98f21ce055
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-25 18:21:57 -0300

    Update Carousel - Imagem de teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,32 +42,32 @@ function App() {
           </ol>
 
           <div className="carousel-inner">      
-          <div className="carousel-item active">
-              <div className="container">
-                  <h1>Slide 1</h1>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                  </p>
-              </div>
-          </div>
+            <div className="carousel-item active carousel-fundo1">
+                <div className="container">
+                    <h1>Slide 1</h1>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+            </div>
 
-          <div className="carousel-item">
-              <div className="container">
-                  <h1>Slide 2</h1>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                  </p>
-              </div>
-          </div>        
+            <div className="carousel-item carousel-fundo2">
+                <div className="container">
+                    <h1>Slide 2</h1>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+            </div>        
 
-          <div className="carousel-item">
-              <div className="container">
-                  <h1>Slide 3</h1>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                  </p>
-              </div>
-          </div>        
+            <div className="carousel-item carousel-fundo3">
+                <div className="container">
+                    <h1>Slide 3</h1>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+                </div>
+            </div>        
         </div>
 
         <a class="carousel-control-prev" data-bs-target="#carouselControle" data-bs-slide="prev">
