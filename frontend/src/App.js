1dffe71f46599d28923289bb27b1c213c967b4b2
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-28 18:59:01 -0300

    Inserindo seção conheça mais sobre o projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,11 +60,25 @@ function App() {
         </a>
       </div>
     </section>
-
+    
+    {/*Conheça mais sobre o projeto*/}
+    <section id="conhecaMais">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+              vídeo
+          </div>
+          <div className="col-md-6">
+              <h2 className="display-6">Conheça mais sobre o projeto</h2>
+              <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </div>
+        </div>
+      </div>
+    </section>
 
       {/*Depoimentos*/}
     
-      <section id="depoimento" className="d-flex">
+      <section id="depoimento" className="d-flex bg-light">
       <div className="container align-self-center">
         <div className="row">
           <h2 className="display-4 pb-4">DEPOIMENTOS</h2>
