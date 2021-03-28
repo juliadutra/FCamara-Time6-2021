406201fe5aa5b14b282e43bc318c79c3d4e3c422
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-28 18:30:23 -0300

    Update menu - inserindo acessibiidade

--- frontend/src/Cabecalho.js
+++ frontend/src/Cabecalho.js
@@ -19,10 +19,10 @@ export default function Cabecalho() {
                     <div id="nav-principal" className="collapse navbar-collapse">
                         <ul className="navbar-nav ms-auto" id="navegacao">
                             <li className="home nav-item"><Link className="nav-link" to="/sobre-nos">Sobre Nós</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/projeto">O Projeto</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/como-doar">Como Doar</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/parceiros">Parceiros</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/fale-conosco">Fale Conosco</Link></li>
+                            <li className="projeto nav-item"><Link className="nav-link" to="/projeto">O Projeto</Link></li>
+                            <li className="doador nav-item"><Link className="nav-link" to="/como-doar">Como Doar</Link></li>
+                            <li className="parceiros nav-item"><Link className="nav-link" to="/parceiros">Parceiros</Link></li>
+                            <li className="faleconosco nav-item"><Link className="nav-link" to="/fale-conosco">Fale Conosco</Link></li>
                         </ul>
                     </div>
                 </div>
--- frontend/src/Parceiros.js
+++ frontend/src/Parceiros.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Cabecalho from './Cabecalho';
 export default function Parceiros() {
     return (
-      <div>
+      <div id="parceiros">
         <Cabecalho/>
         <h1>Parceiros</h1>
       </div>
