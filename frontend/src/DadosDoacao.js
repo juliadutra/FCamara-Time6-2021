f913eccfb0297fa678861fe08e11ec0e179662c0
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-29 18:37:56 -0300

    add: opção de doação para escola específica

--- frontend/src/DadosDoacao.js
+++ frontend/src/DadosDoacao.js
@@ -7,6 +7,8 @@ import Municipios from "./Municipios";
 export default function DadosDoacao() {
 
     const [municipiosDaUF, setMunicipiosDaUF] = useState(null)
+    const [municipioSelecionado, setMunicipioSelecionado] = useState(null)
+    const [seEscolaEspecifica, setSeEscolaEspecifica] = useState(null)
 
     async function aoAlterarUF(evento) {
         const siglaUF = evento.target.value
@@ -19,10 +21,41 @@ export default function DadosDoacao() {
         } else {
             setMunicipiosDaUF([])
         }
+        setMunicipioSelecionado(null)
+        setSeEscolaEspecifica(null)
 
     }
 
+    function aoAlterarMunicipio(evento) {
+        const municipioSelecionado = evento.target.value
+        setMunicipioSelecionado(municipioSelecionado)
+        setSeEscolaEspecifica(null)
+    }
 
+    function aoClicarSimEscolaEspecifica() {
+        setSeEscolaEspecifica(true)
+    }
+
+    function aoClicarNaoEscolaEspecifica() {
+        setSeEscolaEspecifica(false)
+    }
+
+    let classesBotaoSim = "btn "
+    let classesBotaoNao = "btn "
+    
+    if (seEscolaEspecifica === null) {
+        console.log(1)
+        classesBotaoSim = classesBotaoSim + " btn-outline-dark"
+        classesBotaoNao = classesBotaoNao + " btn-outline-dark"
+    } else if(seEscolaEspecifica) {
+        console.log(2)
+        classesBotaoSim = classesBotaoSim + " btn-primary"
+        classesBotaoNao = classesBotaoNao + " btn-outline-dark"
+    } else {
+        console.log(3)
+        classesBotaoSim = classesBotaoSim + " btn-outline-dark"
+        classesBotaoNao = classesBotaoNao + " btn-primary"
+    }
 
     return (
         <div className="card m-4">
@@ -32,7 +65,17 @@ export default function DadosDoacao() {
             <div className="card-body">
                 <div>
                     <UFs onChange={aoAlterarUF} />
-                    <Municipios lista={municipiosDaUF} />
+                    <Municipios lista={municipiosDaUF} onChange={aoAlterarMunicipio} />
+                    {
+                        municipioSelecionado && (
+                            <>
+                                <label htmlFor="seEscolaEspecifica" className="form-label">Deseja doar para uma escola específica deste município?</label>
+                                <button className={classesBotaoSim} onClick={aoClicarSimEscolaEspecifica}>Sim</button>
+                                <button className={classesBotaoNao} onClick={aoClicarNaoEscolaEspecifica}>Não</button>
+                            </>
+                        )
+                    }
+                    
                 </div>
             </div>
         </div>
