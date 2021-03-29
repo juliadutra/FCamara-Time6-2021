026eb8437f7bf5edf6185b6f4411f6acf4a60be6
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-29 01:07:31 -0300

    add: campo de cpf com validação e máscara

--- frontend/src/DadosSolicitacao.js
+++ frontend/src/DadosSolicitacao.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
-import UFs from "./UFs"
-import Municipios from "./Municipios";
+import { validate } from 'gerador-validador-cpf';
 
-
+function mascaraCpf(valor) {
+    return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"\$1.\$2.\$3\-\$4");
+}
 
 export default function DadosSolicitacao() {
+    const [ cpf, setCPF ] = useState("")
+    const [ cpfValido, setCPFValido ] = useState(true)
+    function aoAlterarCPF(event) {
+        const cpf = event.target.value
+        const cpfValido = validate(cpf)
+        setCPF(cpf)
+        setCPFValido(cpfValido)
+    }
+
+
+    const exibirMensagemCPFInvalido = cpf.length > 10 && !cpfValido
     return (
         <div className="card m-4">
             <div className="card-header">
@@ -12,7 +24,24 @@ export default function DadosSolicitacao() {
             </div>
             <div className="card-body">
                 <div>
-                    Formulário de solicitação
+                    <label htmlFor="cpf" className="form-label">Informe o seu CPF</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        style={{ maxWidth: 200 }}
+                        maxLength={14}
+                        onChange={aoAlterarCPF}
+                        placeholder="Ex.: 078.840.390-79"
+                        value={mascaraCpf(cpf)}
+                    />
+                    {
+                        exibirMensagemCPFInvalido && (
+                            <div className="mt-2 text-danger">
+                                Por favor, informe um CPF válido
+                            </div>
+                        )
+                    }
+                    
                 </div>
             </div>
         </div>
