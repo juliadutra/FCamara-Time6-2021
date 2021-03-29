2f69a5146de0e6488fd336907f4db89b2169ac3b
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-28 22:21:23 -0300

    Criação estrutura o projeto

--- frontend/src/Projeto.js
+++ frontend/src/Projeto.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Cabecalho from './Cabecalho';
+import './Projeto.css';
 
 export default function Projeto() {
     return (
       <div id="projeto">
         <Cabecalho/>
-        <h1>O Projeto</h1>
+        
+      {/*Nossa história*/}
+      <section id="historia">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <h1 className="display-6">Nossa história</h1>
+              <p className="lead pt-4">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              </p>
+            </div>
+            <div className="col-md-6">
+              carousel
+            </div>
+          </div>
+        </div>
+      </section>
       </div>
     );
   }
