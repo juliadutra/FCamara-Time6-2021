61e03ff8ab1215c88bf4ed61ada22276c7a470e6
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-30 12:23:58 -0300

    Update fale conosco - Criação do formulário e textos

--- frontend/src/FaleConosco.js
+++ frontend/src/FaleConosco.js
@@ -10,9 +10,9 @@ export default function FaleConosco() {
           
 
         {/*Capa*/}
-        <section id="capaFaleConosco">
-          <div className="container">
-            <h1 className="display-4 text-center">FALE CONOSCO</h1>
+        <section id="capaFaleConosco" className="d-flex">
+          <div className="container align-self-center">
+            <h1 className="display-4 text-center">Fale conosco</h1>
           </div>
         </section>
 
@@ -22,10 +22,51 @@ export default function FaleConosco() {
           <div className="container">
             <div className="row">
               <div className="col-md-6">
-                form
+                <form>
+                  <fieldset>
+                    <legend className="display-6">Formulário</legend>
+                    <div className="form-group mb-4">
+                      <label for="usuario">Seu nome completo</label>
+                      <input className="form-control form-control" type="text" id="usuario" placeholder="Ex. João da Silva" />
+                    </div>
+
+                    <div className="form-group mb-4">
+                      <label for="email">Digite seu email</label>
+                      <input className="form-control form-control" type="email" id="email" placeholder="Ex. joao@email.com" />
+                    </div>
+
+                    <div className="form-group mb-4">
+                      <label for="telefone">Digite seu telefone</label>
+                      <input className="form-control form-control" type="text" id="telefone" placeholder="Ex. (99) 99999-9999" />
+                    </div>
+
+                    <div className="form-group mb-4">
+                      <label for="assunto">Assunto</label>
+                      <input className="form-control form-control" type="text" id="assunto" placeholder="Ex. Doação mensal" />
+                    </div>
+
+                    <div className="form-group mb-4">
+                      <label for="mensagem">Assunto</label>
+                      <textarea className="form-control" id="mensagem"  rows="3" placeholder="Digite aqui a sua mensagem"></textarea>
+                    </div>
+
+                    <button type="button" className="btn btn-outline-dark">Enviar</button>
+                  </fieldset>
+                </form>
               </div>
+
+              {/*Contato*/}   
               <div className="col-md-6">
-                texto
+                <h1 className="display-6">Nossos Contatos</h1>                             
+                <p className="lead">
+                  <i class="fas fa-phone-alt"></i> <b>Telefone:</b> (99) 99999-9999<br/>                                    
+                </p>
+                <p className="lead">
+                  <i class="fab fa-whatsapp"></i> <b>WhatsApp:</b> (99) 99999-9999<br/>
+                </p>
+                <p className="lead">
+                  <i class="far fa-envelope"></i> <b>E-Mail:</b> informacoes@padrinhosdobem.com<br/>
+                </p>
               </div>
             </div>
           </div>
