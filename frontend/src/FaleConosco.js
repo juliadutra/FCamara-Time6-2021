3fd995bbad2bb50a1287be360526f68c00e83cfa
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-30 10:57:02 -0300

    Criação da estrutura fale conosco

--- frontend/src/FaleConosco.js
+++ frontend/src/FaleConosco.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import Cabecalho from './Cabecalho';
+import './FaleConosco.css';
+
 export default function FaleConosco() {
     return (
       <div id="faleconosco">
         <Cabecalho/>
-        <h1>Fale Conosco</h1>
+        {/*<h1>Fale Conosco</h1>*/}
+          
+
+        {/*Capa*/}
+        <section id="capaFaleConosco">
+        capa
+        </section>
+
+
+        {/*Texto e formulário*/}
+        <section id="faleConosco">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                form
+              </div>
+              <div className="col-md-6">
+                texto
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
