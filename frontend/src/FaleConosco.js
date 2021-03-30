c85fbc0f9481f3cf657408db7a2fa3af2062144c
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-30 13:00:39 -0300

    Update fale conosco - Ajustes finais no botão

--- frontend/src/FaleConosco.js
+++ frontend/src/FaleConosco.js
@@ -50,7 +50,8 @@ export default function FaleConosco() {
                       <textarea className="form-control" id="mensagem"  rows="3" placeholder="Digite aqui a sua mensagem"></textarea>
                     </div>
 
-                    <button type="button" className="btn btn-outline-dark">Enviar</button>
+                    <button type="button" className="btn btn-outline-dark justify-content-md-center">Enviar</button>
+                   
                   </fieldset>
                 </form>
               </div>
