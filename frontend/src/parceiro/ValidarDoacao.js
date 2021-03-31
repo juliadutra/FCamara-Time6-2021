417d5728924d4cd46a417bb521751183bb455bec
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-31 20:13:24 -0300

    Add: campo para informar código do ticket

--- frontend/src/parceiro/ValidarDoacao.js
+++ frontend/src/parceiro/ValidarDoacao.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 import Cabecalho from "../Cabecalho"
 
 export default function ValidarDoacao() {
+    const [codigoTicket, setCodigoTicket] = useState("")
+    function aoAlterarCodigoTicket(evento) {
+        setCodigoTicket(evento.target.value)
+    }
     return (
         <>
             <Cabecalho />
-            <h1>Validar Doação</h1>
+            <div className="container">
+                <h3>Validar Doação</h3>
+                <h6>Informe o código do ticket que deseja validar e a senha de validação</h6>
+                <div className="row">
+                    <div className="col-lg-8">
+                        <div className="mb-3">
+                            <label className="form-label">Código do ticket</label>
+                            <input
+                                className="form-control"
+                                value={codigoTicket}
+                                onChange={aoAlterarCodigoTicket}
+                                style={{ maxWidth: 350 }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
