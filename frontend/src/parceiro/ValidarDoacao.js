7a54e41c91591dabd45f86b7b6c61037e442ef19
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-31 20:44:05 -0300

    Add: comportamento básico para validar ticket

--- frontend/src/parceiro/ValidarDoacao.js
+++ frontend/src/parceiro/ValidarDoacao.js
@@ -1,14 +1,38 @@
 import React, {useState} from "react"
 import Cabecalho from "../Cabecalho"
+import { useAlert } from "react-alert"
 
 export default function ValidarDoacao() {
     const [codigoTicket, setCodigoTicket] = useState("")
     const [chaveValidacao, setChaveValidacao] = useState("")
+    const alert = useAlert()
     function aoAlterarCodigoTicket(evento) {
         setCodigoTicket(evento.target.value)
     }
     function aoAlterarChaveValidacao(evento) {
         setChaveValidacao(evento.target.value)
+    }
+    
+    async function aoClicarValidar() {
+        if (codigoTicket === "" || chaveValidacao === "") {
+            alert.info("Por favor, informe o código do ticket e a chave de validação.")
+        } else {
+            const urlTicket = "https://doacao-de-material-escolar-default-rtdb.firebaseio.com/tickets/" + codigoTicket + ".json"
+            const ticket = await fetch(urlTicket)
+            const ticketJson = await ticket.json()
+            if (ticketJson === null) {
+                alert.error("Nenhum ticket encontrado com o código " + codigoTicket)
+            } else {
+                const urlParceiro = "https://doacao-de-material-escolar-default-rtdb.firebaseio.com/parceiros/" + chaveValidacao + ".json"
+                const parceiro = await fetch(urlParceiro)
+                const parceiroJson = await parceiro.json()
+                if(parceiroJson === null) {
+                    alert.error("Chave de validação incorreta")
+                } else {
+                    alert.info("Tudo certo para validar...")
+                }
+            }
+        }
     }
     return (
         <>
@@ -36,6 +60,13 @@ export default function ValidarDoacao() {
                                 onChange={aoAlterarChaveValidacao}
                                 style={{ maxWidth: 350 }}
                             />
+                        </div>
+                        <div className="mb-3">
+                            <button
+                                className="btn btn-outline-primary"
+                                onClick={aoClicarValidar}>
+                                Validar
+                            </button>
                         </div>
                     </div>
                 </div>
