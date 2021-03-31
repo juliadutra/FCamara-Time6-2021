9f27cb74aa52ab333c3280c4544e5e139c17fccc
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-31 18:27:20 -0300

    Update parceiros - textos e formatações

--- frontend/src/Parceiros.js
+++ frontend/src/Parceiros.js
@@ -26,6 +26,37 @@ export default function Parceiros() {
           </div>
         </section>
 
+        <section>
+          <h1 className="display-6 pt-4 text-center">Alguns de nossos parceiros</h1>
+
+          <div className="container pt-4">
+            <div className="row">
+              <div className="col-md-6">
+                  <p className="lead">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  </p>
+              </div>
+              <div id="fundoParceria" className="col-md-6">
+                imagem logo         
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="tonarParceiro" className="d-flex">
+          <div className="container align-self-center">
+            <div className="row">
+              <div className="col-md">
+                  <h1 className="display-6 pt-4 text-center pb-4">Seja um parceiro você também</h1>
+          
+                <div class="d-grid gap-2 d-flex justify-content-center">
+                  <button class="btn btn-outline-dark" type="button">Quero me tornar um parceiro</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </div>
     );
   }
