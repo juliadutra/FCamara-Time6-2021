c9e635ec43b3699575b58941fe0d04e50ba09be3
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-31 17:14:31 -0300

    Criação da estrutura  Parceiros

--- frontend/src/Parceiros.js
+++ frontend/src/Parceiros.js
@@ -4,7 +4,12 @@ export default function Parceiros() {
     return (
       <div id="parceiros">
         <Cabecalho/>
-        <h1>Parceiros</h1>
+        {/*<h1>Parceiros</h1>*/}
+
+        <section>
+          <h1 className="display-6 pt-4 text-center">Entenda como funciona nossa parceria</h1>
+        </section>
+
       </div>
     );
   }
