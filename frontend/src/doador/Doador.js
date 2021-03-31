80a26617bbdf938a1fb896f28990d7e5d6ed4108
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-03-31 11:29:15 -0300

    Removendo código comentado

--- frontend/src/doador/Doador.js
+++ frontend/src/doador/Doador.js
@@ -70,25 +70,7 @@ export default function Doador() {
         </div>
 
       </section>
-
-      {/*
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="card">
-              <div id="fundocard"></div>
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-          </div>
-          </div>
-        </div>
-      </div>
-      */}
-
-        
+   
     </div>
   );
 }
