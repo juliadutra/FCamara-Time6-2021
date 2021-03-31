2b5228c3524b0a5184aeccea8e9e946c7995e014
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-30 21:46:18 -0300

    Add: campo para informar número do ticket

--- frontend/src/parceiro/ValidarDoacao.js
+++ frontend/src/parceiro/ValidarDoacao.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import Cabecalho from "../Cabecalho"
 
 export default function ValidarDoacao() {
+    const [numeroTicket, setNumeroTicket] = useState("")
+    function aoAlterarNumeroTicket(evento) {
+        setNumeroTicket(evento.target.value)
+    }
     return (
         <>
             <Cabecalho />
-            <div>
-                <h1>Validar Doação</h1>
+            <div className="container">
+                <h1>Consultar Ticket</h1>
+                <div className="row">
+                    <div className="col-lg-8">
+                        <div className="mb-3">
+                            <label className="form-label">Informe o número do ticket</label>
+                            <input
+                                value={numeroTicket}
+                                onChange={aoAlterarNumeroTicket}
+                                className="form-control"
+                                style={{ maxWidth: 350 }}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
