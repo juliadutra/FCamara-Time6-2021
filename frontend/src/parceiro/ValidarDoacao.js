fb32b02ffa318ad66be0ad2e9459301fe0aeee7f
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-30 22:28:08 -0300

    Add: carregando ao clicar em pesquisar

--- frontend/src/parceiro/ValidarDoacao.js
+++ frontend/src/parceiro/ValidarDoacao.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react"
 import Cabecalho from "../Cabecalho"
 import { useAlert } from "react-alert"
+import Carregando from "../Carregando"
 
 export default function ValidarDoacao() {
     const [numeroTicket, setNumeroTicket] = useState("")
+    const [exibirCarregando, setExibirCarregando] = useState(false)
     const alert = useAlert()
     function aoAlterarNumeroTicket(evento) {
         setNumeroTicket(evento.target.value)
@@ -11,8 +13,10 @@ export default function ValidarDoacao() {
 
     async function aoClicarConsultarTicket() {
         const url = "https://doacao-de-material-escolar-default-rtdb.firebaseio.com/tickets/" + numeroTicket + ".json"
+        setExibirCarregando(true)
         const resposta = await fetch(url)
         const respostaJson = await resposta.json()
+        setExibirCarregando(false)
         if(respostaJson === null) {
             alert.error("Nenhum ticket encontrado com o código " + numeroTicket)
         } else {
@@ -23,6 +27,7 @@ export default function ValidarDoacao() {
     return (
         <>
             <Cabecalho />
+            <Carregando exibir={exibirCarregando} />
             <div className="container">
                 <h1>Consultar Ticket</h1>
                 <div className="row">
