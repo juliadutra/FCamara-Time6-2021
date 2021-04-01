d30fda9ca1fe28258e5939d17435a3824ebef7f7
Julia Dutra <juliadutragoncalves@gmail.com>
2021-03-31 23:14:10 -0300

    recriei arquivo que estava com problema no nome

---
+++ frontend/src/NomeIndefinido.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Cabecalho from './Cabecalho';
+
+
+export default function NomeIndefinido() {
+    return (
+        <div>
+          <Cabecalho/>   
+          
+          <section>
+            CADASTRE-SE OU ACOMPANHE SUAS SOLICITAÇÕES AQUI
+          </section>
+
+
+        </div>
+  );
+}
