db56351759da46ce6577c213715f936e72a83f41
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-04-02 16:26:36 -0300

    Update sobre nós - removendo carousel slide e inserindo capa de fundo

--- frontend/src/Sobre.js
+++ frontend/src/Sobre.js
@@ -8,133 +8,101 @@ export default function Sobre() {
       <div id="sobreNos">
         <Cabecalho/>
        {/* <h1>Sobre Nós</h1>*/}
+    
+      {/*Foto da capa*/}
+      <section id="capaSobre">
+      </section>     
 
-        {/*Nossa história*/}
-  <section id="historia">
-    <div className="container">
-      <div className="row mt-4">
-        <div className="col-md-4">
-          <h1 className="display-6">Como tudo começou?</h1>
-            <p className="lead pt-4">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            </p>
+      {/*Nossa história*/}
+      <section id="historia">
+        <div className="container text-center">
+          <div className="row mt-4">
+            <div className="col-md-12">
+              <h1 className="display-6">Como tudo começou?</h1>
+                <p className="lead pt-4 d-flex justify-content-center">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                </p>
+            </div>                
         </div>
-        {/*Carousel*/}
-        <div className="col-md-8">
-          <div id="carouselControle" className="carousel slide carousel-fade" data-bs-ride="carousel" >
-            <ol className="carousel-indicators">
-              <li data-bs-target="#carouselControle" data-bs-slide-to="0" className="active"></li>
-              <li data-bs-target="#carouselControle" data-bs-slide-to="1"></li>
-              <li data-bs-target="#carouselControle" data-bs-slide-to="2"></li>
-            </ol>
+        </div>
+      </section>
 
-          <div className="carousel-inner">      
-            <div className="carousel-item active carousel-fundo1" data-bs-interval="6000">
+      {/*como estamos hoje*/}
+      <section id="situacaoAtual" className="bg-light">
+        <div className="container-fluid">
+          <h1 className="display-4 text-center">Como estamos hoje ?</h1>
+          <div className="row">
+            <div className="col-sm">
+              <div className="d-flex justify-content-around text-center">
+                <div className="pe-2">
+                  <h3>250</h3>
+                  <p className="lead">Crianças alcançadas</p>
+                </div>
+
+                <div className="pe-2">
+                  <h3>250</h3>
+                  <p className="lead">Parceiros</p>
+                </div>
+
+                <div className="pe-2">
+                  <h3>250</h3>
+                  <p className="lead">Kits arrecadados</p>
+                </div>
               
+                <div className="pe-2">
+                  <h3>250</h3>
+                  <p className="lead">Crianças esperando doação</p>
+                </div>
+
+                <div className="pe-2">
+                  <h3>250</h3>
+                  <p className="lead">Estados alcançados</p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/*missão, visão e valores */}
+      <section id="lema" className="text-center">
+        <h1 className="display-4">Nosso lema</h1>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <div class="card">
+                <h3 className="text-center">Missão</h3>
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
             </div>
 
-            <div className="carousel-item carousel-fundo2" data-bs-interval="6000">
-                
-            </div>        
+            <div className="col-md-4">
+              <div class="card">
+              <h3 className="text-center">Visão</h3>
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
 
-            <div className="carousel-item carousel-fundo3" data-bs-interval="6000">
-              
-            </div>        
+            <div className="col-md-4">
+              <div class="card">
+              <h3 className="text-center">Valores</h3>
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+
           </div>
-
-          <a class="carousel-control-prev" data-bs-target="#carouselControle" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </a>
-
-          <a class="carousel-control-next" data-bs-target="#carouselControle" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </a>
         </div>
-      </div>
+      </section>
+
     </div>
-    </div>
-  </section>
-
-  {/*como estamos hoje*/}
-  <section id="situacaoAtual" className="bg-light">
-    <div className="container-fluid">
-      <h1 className="display-4 text-center">Como estamos hoje ?</h1>
-      <div className="row">
-        <div className="col-sm">
-          <div className="d-flex justify-content-around text-center">
-            <div className="pe-2">
-              <h3>250</h3>
-              <p className="lead">Crianças alcançadas</p>
-            </div>
-
-            <div className="pe-2">
-              <h3>250</h3>
-              <p className="lead">Parceiros</p>
-            </div>
-
-            <div className="pe-2">
-              <h3>250</h3>
-              <p className="lead">Kits arrecadados</p>
-            </div>
-          
-            <div className="pe-2">
-              <h3>250</h3>
-              <p className="lead">Crianças esperando doação</p>
-            </div>
-
-            <div className="pe-2">
-              <h3>250</h3>
-              <p className="lead">Estados alcançados</p>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-
-  {/*missão, visão e valores */}
-  <section id="lema" className="text-center">
-    <h1 className="display-4">Nosso lema</h1>
-
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-4">
-          <div class="card">
-            <h3 className="text-center">Missão</h3>
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div class="card">
-          <h3 className="text-center">Visão</h3>
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4">
-          <div class="card">
-          <h3 className="text-center">Valores</h3>
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </section>
-
-
-
-</div>
     );
   }
