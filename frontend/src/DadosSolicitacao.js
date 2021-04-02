85c02cc418522f66c46312a7a6a1d86f23d6785a
Julia Dutra <juliadutragoncalves@gmail.com>
2021-04-02 08:53:41 -0300

    fix warnings

--- frontend/src/DadosSolicitacao.js
+++ frontend/src/DadosSolicitacao.js
@@ -3,7 +3,7 @@ import { validate } from 'gerador-validador-cpf';
 import { useAlert } from "react-alert";
 
 function mascaraCpf(valor) {
-    return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"\$1.\$2.\$3\-\$4");
+    return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"$1.$2.$3-$4");
 }
 
 export default function DadosSolicitacao() {
