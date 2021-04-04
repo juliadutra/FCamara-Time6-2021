f92e438d7b36e15719d685ede614e61bb2a2bd96
Julia Dutra <juliadutragoncalves@gmail.com>
2021-04-04 11:43:30 -0300

    Juntei o conteúdo dos componentes para estruturar melhor a exibição dos resultados da consulta

--- frontend/src/responsavel/CadastrarPedido.js
+++ frontend/src/responsavel/CadastrarPedido.js
@@ -1,22 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { validate } from 'gerador-validador-cpf';
+import { useAlert } from "react-alert";
 import Cabecalho from "../Cabecalho";
-import DadosSolicitacao from "./DadosSolicitacao";
+
+function mascaraCpf(valor) {
+    return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"$1.$2.$3-$4");
+}
 
 export default function CadastrarPedido() {
+    const alert = useAlert()
+    const [cpf, setCPF] = useState("")
+    const [solicitacoes, setSolicitacoes] = useState(null)
+    function aoAlterarCPF(event) {
+        const cpf = event.target.value
+        setCPF(cpf)
+    }
+
+    async function aoClicarEmRecuperarSolicitacoes() {
+        const cpfValido = validate(cpf)
+        if (!cpfValido) {
+            alert.error("O CPF informado não é válido")
+            return
+        }
+
+        const cpfBuscar = cpf.replace(".", "").replace("-", "")
+        const url = "https://doacao-de-material-escolar-default-rtdb.firebaseio.com/cpfs/" + cpfBuscar + ".json"
+        const resultado = await fetch(url)
+        const cpfJSON = await resultado.json()
+        if (cpfJSON === null) {
+            alert.error("Não há registro de solicitações para o CPF informado")
+        } else {
+            setSolicitacoes(cpfJSON.solicitacoes)
+        }
+
+    }
+
+    function aoClicarEmNovaSolicitacao() {
+        alert.show("Clicou em nova solicitação")
+    }
+
+    const exibirMensagemCPFInvalido = cpf.length > 10 && !validate(cpf)
     return (
         <>
             <Cabecalho />
+
             <div className="container">
                 <div className="leader p-4">
                     Quero Solicitar Kit de Material Escolar
                 </div>
                 <div className="row">
                     <div className="col-lg-8">
-                        <DadosSolicitacao />
+                        <div className="card m-4">
+                            <div className="card-header">
+                                <h4 className="my-0 font-weight-normal">Dados da Solicitação</h4>
+                            </div>
+                            <div className="card-body">
+                                <div>
+                                    <p>Digite o seu CPF para registrar uma nova solicitação de Kit Escolar ou para consultar solicitações já registradas</p>
+                                    <label htmlFor="cpf" className="form-label">Informe o seu CPF</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        style={{ maxWidth: 350 }}
+                                        maxLength={14}
+                                        onChange={aoAlterarCPF}
+                                        placeholder="Ex.: 078.840.390-79"
+                                        value={mascaraCpf(cpf)}
+                                    />
+                                    {
+                                        exibirMensagemCPFInvalido && (
+                                            <div className="mt-2 text-danger">
+                                                Por favor, informe um CPF válido
+                                            </div>
+                                        )
+                                    }
+
+                                    <button className="btn btn-primary btn-sm btn-block" onClick={aoClicarEmRecuperarSolicitacoes}>
+                                        Recuperar Solicitações
+                                    </button>
+                                    <button className="btn btn-secondary btn-sm btn-block" onClick={aoClicarEmNovaSolicitacao}>
+                                        Nova Solicitação
+                                    </button>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            
+
         </>
     )
 }
--- frontend/src/responsavel/DadosSolicitacao.js
+++ frontend/src/responsavel/DadosSolicitacao.js
@@ -1,81 +1,8 @@
-import React, { useState } from "react";
-import { validate } from 'gerador-validador-cpf';
-import { useAlert } from "react-alert";
-
-function mascaraCpf(valor) {
-    return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,"$1.$2.$3-$4");
-}
+import React from "react"
 
 export default function DadosSolicitacao() {
-    const alert = useAlert()
-    const [ cpf, setCPF ] = useState("")
-    const [ solicitacoes, setSolicitacoes ] = useState(null)
-    function aoAlterarCPF(event) {
-        const cpf = event.target.value
-        setCPF(cpf)
-    }
-
-    async function aoClicarEmRecuperarSolicitacoes() {
-        const cpfValido = validate(cpf)
-        if (!cpfValido) {
-            alert.error("O CPF informado não é válido")
-            return
-        }
-
-        const cpfBuscar = cpf.replace(".", "").replace("-", "")
-        const url = "https://doacao-de-material-escolar-default-rtdb.firebaseio.com/cpfs/" + cpf + ".json"
-        const resultado = await fetch(url)
-        const cpfJSON = await resultado.json()
-        if (cpfJSON === null) {
-            alert.error("Não há registro de solicitações para o CPF informado")
-        } else {
-            setSolicitacoes(cpfJSON.solicitacoes)
-        }
-
-    }
-    
-    function aoClicarEmNovaSolicitacao() {
-        alert.show("Clicou em nova solicitação")
-    }
-
-
-    const exibirMensagemCPFInvalido = cpf.length > 10 && !validate(cpf)
     return (
-        <div className="card m-4">
-            <div className="card-header">
-                <h4 className="my-0 font-weight-normal">Dados da Solicitação</h4>
-            </div>
-            <div className="card-body">
-                <div>
-                    <p>Digite o seu CPF para registrar uma nova solicitação de Kit Escolar ou para consultar solicitações já registradas</p>
-                    <label htmlFor="cpf" className="form-label">Informe o seu CPF</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        style={{ maxWidth: 350 }}
-                        maxLength={14}
-                        onChange={aoAlterarCPF}
-                        placeholder="Ex.: 078.840.390-79"
-                        value={mascaraCpf(cpf)}
-                    />
-                    {
-                        exibirMensagemCPFInvalido && (
-                            <div className="mt-2 text-danger">
-                                Por favor, informe um CPF válido
-                            </div>
-                        )
-                    }
-
-                    <button className="btn btn-primary btn-sm btn-block" onClick={aoClicarEmRecuperarSolicitacoes}>
-                        Recuperar Solicitações
-                    </button>
-                    <button className="btn btn-secondary btn-sm btn-block" onClick={aoClicarEmNovaSolicitacao}>
-                        Nova Solicitação
-                    </button>
-                    
-                </div>
-            </div>
-        </div>
+        <h1>Dados da solicitação</h1>
     )
 
 }
