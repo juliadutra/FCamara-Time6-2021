b51893b540fbd41a0c7eb07cb84ef09a8987c5bc
GiovannaSantos99 <giovanna.santos99@yahoo.com>
2021-04-04 21:26:29 -0300

    Update home - Mundando borda e inserindo o background

--- frontend/src/Cabecalho.js
+++ frontend/src/Cabecalho.js
@@ -1,18 +1,19 @@
 import React from "react"
 import {Link} from "react-router-dom";
+import './Cabecalho.css';
 
 
 export default function Cabecalho() {
     return (
 
-        <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <header>
+        <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#controleNavegacao" aria-controls="controleNavegacao" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <Link className="navbar-brand" to="/">
-                    <h1>Logo</h1>
+                    <h1 className="text-light">Logo</h1>
                 </Link>
                 <div className="collapse navbar-collapse" id="controleNavegacao">
                 <ul id="navegacao" className="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -25,7 +26,7 @@ export default function Cabecalho() {
                 </div>
             </div>
         </nav>
-        </header>
+    </header>
 
 
     )
