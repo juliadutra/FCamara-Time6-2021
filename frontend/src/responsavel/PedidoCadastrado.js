8f80b808a45b74de18c55522227c06f0636a5bcb
Julia Dutra <juliadutragoncalves@gmail.com>
2021-04-04 23:01:08 -0300

    Exibindo código do pedido apos finalizar

--- frontend/src/responsavel/PedidoCadastrado.js
+++ frontend/src/responsavel/PedidoCadastrado.js
@@ -5,8 +5,19 @@ export default function PedidoCadastrado(props) {
         return null
     } else {
         return (
-            <div>
-                Pedido cadastrado com sucesso.
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 p-4">
+                        <h4>Pedido cadastrado com sucesso.</h4>
+                        <h6>A seguir estão os dados da sua solicitação, você pode consultar todos os pedidos a partir do seu CPF no futuro.</h6>
+                        <div className="card">
+                            <div className="card-header">
+                                Pedido nᵒ {props.solicitacaoCadastrada.codigoPedido}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         )
     }
